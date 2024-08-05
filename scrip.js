16f4f80596036843cbeae7d6047e2b02b5e6c220
xeenlight <vovabvd@gmail.com>
2024-08-06 02:53:32 +0500

    3

--- scrip.js
+++ scrip.js
@@ -1,6 +1,6 @@
 function codeCountry() {
-    const country = document.getElementById("countryS").value;
-    const phone = document.getElementById("phone");
+    let country = document.getElementById("countryS").value;
+    let phone = document.getElementById("phone");
 
     if (country === "none") {
         phone.value = "Выберите страну";
@@ -17,3 +17,35 @@ function toggleSubmitButton() {
     const submitButton = document.getElementById("submitButton");
     submitButton.disabled = !checkbox.checked;
 }
+function CalcCurse() {
+    let courseSelect = document.getElementById("courseSelect");
+    let discountRange = document.getElementById("discountRange");
+    let discountValue = document.getElementById("discountValue");
+    let sumValue = document.getElementById("sumValue");
+
+    let course = courseSelect.value;
+    let Frontend_Price = 14000000;
+    let Python_Price = 12000000;
+    let DATA_Price = 16000000;
+
+    let price;
+
+    if (course === "none") {
+        price = 0;
+    } else if (course === "Frontend") {
+        price = Frontend_Price;
+    } else if (course === "Python") {
+        price = Python_Price;
+    } else if (course === "DATA") {
+        price = DATA_Price;
+    }
+
+    let discount = parseInt(discountRange.value);
+    let finalPrice = price - (price * (discount / 100));
+
+    sumValue.value = finalPrice.toLocaleString(); 
+
+    discountValue.textContent = `${discount}%`;
+}
+document.getElementById("courseSelect").addEventListener("change", CalcCurse);
+document.getElementById("discountRange").addEventListener("input", CalcCurse);
